36a4c421c303bf686419773df946f83382d87f28
Taco <1141374791@qq.com>
2021-01-10 23:07:57 +0800

    bookmark

--- create.sql
+++ create.sql
@@ -1,91 +1,84 @@
 create database bookstore
 go
+
 use bookstore
+create table DBUSER
+(
+    Username VARCHAR(30) not null,
+    Password VARCHAR(30) not null,
+    primary key(Username)
+)
 
 CREATE TABLE BSUSER
 (
     Username VARCHAR(15) NOT NULL,
     Password VARCHAR(15) NOT NULL,
-    Email VARCHAR(20) NOT NULL,
+    Address VARCHAR(20) NOT NULL,
     PRIMARY KEY(Username)
 );
 
 CREATE TABLE AUTHOR
 (
     AuthorID VARCHAR(15) NOT NULL,
-    Authorname VARCHAR(15) NOT NULL,
+    AuthorName VARCHAR(15) NOT NULL,
     PRIMARY KEY(AuthorID)
 );
 
 CREATE TABLE BOOK
 (
     ISBN CHAR(14) NOT NULL,
-    Edition VARCHAR(15),
     Title VARCHAR(50) NOT NULL,
     AuthorID VARCHAR(15) NOT NULL,
+    Quantity INT NOT NULL,
+    CHECK (Quantity >= 0),
     PRIMARY KEY(ISBN),
     FOREIGN KEY(AuthorID) references AUTHOR(AuthorID)
 );
 
-CREATE TABLE LISTING
-(
-    Seller VARCHAR(15) NOT NULL,
-    Book CHAR(14) NOT NULL,
-    Price DECIMAL(4,2) NOT NULL,
-    Quantity INT,
-    CHECK 		  (Quantity > 0),
-    PRIMARY KEY(Seller, Book),
-    FOREIGN KEY(Book) references BOOK(ISBN),
-    FOREIGN KEY(Seller) references BSUSER(Username)
-);
-
-CREATE TABLE USER_REVIEW
+CREATE TABLE REVIEW
 (
     Reviewer VARCHAR(15) NOT NULL,
-    Reviewee VARCHAR(15) NOT NULL,
+    BookID CHAR(14) NOT NULL,
     Rating INT NOT NULL,
     Review TEXT,
-    CHECK 		  (Rating >= 1 AND Rating <= 5),
-    PRIMARY KEY(Reviewer, Reviewee),
+    CHECK (Rating >= 1 OR Rating <= 5),
+    PRIMARY KEY(Reviewer, BookID),
     FOREIGN KEY(Reviewer) references BSUSER(Username),
-    FOREIGN KEY(Reviewee) references BSUSER(Username)
-);
-
-CREATE TABLE BOOK_REVIEW
-(
-    Reviewer VARCHAR(15) NOT NULL,
-    Book CHAR(14) NOT NULL,
-    Review TEXT,
-    Rating INT NOT NULL,
-    CHECK 		  (Rating >= 1 OR Rating <= 5),
-    PRIMARY KEY(Reviewer, Book),
-    FOREIGN KEY(Reviewer) references BSUSER(Username),
-    FOREIGN KEY(Book) references BOOK(ISBN)
+    FOREIGN KEY(BookID) references BOOK(ISBN)
 );
 
 CREATE TABLE TRANSACT
 (
-    BuyerUN VARCHAR(15) NOT NULL,
-    SellerUN VARCHAR(15) NOT NULL,
+    Buyer VARCHAR(15) NOT NULL,
     BookID CHAR(14) NOT NULL,
     DateTime CHAR(23) NOT NULL,
-    PRIMARY KEY(BuyerUN, SellerUN, BookID, DateTime),
-    FOREIGN KEY(BuyerUN) references BSUSER(Username),
-    FOREIGN KEY(SellerUN,BookID) references LISTING (Seller, Book)
+    PRIMARY KEY(Buyer, BookID, DateTime),
+    FOREIGN KEY(Buyer) references BSUSER(Username),
+    FOREIGN KEY(BookID) references BOOK(ISBN)
 );
+
+create table BOOKMARK
+(
+    Title VARCHAR(15),
+    Owner VARCHAR(15),
+    BookID CHAR(14),
+    Owned Bit,
+    primary key(Title, Owner, BookID),
+    foreign key(Owner) references BSUSER(Username),
+    foreign key(BookID) references BOOK(ISBN)
+)
 go
-create trigger t_user_reivew on USER_REVIEW for insert as
+
+create trigger t_reivew on REVIEW for insert as
 if (select count(*)
     from TRANSACT t, inserted i
-    where t.BuyerUN=i.Reviewer and t.SellerUN=i.Reviewee)=0
-BEGIN
-rollback TRANSACTION
-end
-go
-create trigger t_book_reivew on BOOK_REVIEW for insert as
-if (select count(*)
-    from TRANSACT t, inserted i
-    where t.BuyerUN=i.Reviewer and t.BookID=i.Book)=0
+    where t.Buyer=i.Reviewer and t.BookID=i.BookID)=0
 begin
 rollback TRANSACTION
 end
+go
+
+create trigger t_transact on TRANSACT for insert as
+update book set quantity=quantity-1 where ISBN=(select BookID from inserted)
+update bookmark set Owned=1 where BookID = (select BookID from inserted)
+and Owner=(select Buyer from inserted)
